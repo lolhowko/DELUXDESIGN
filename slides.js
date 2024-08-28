ea84b30f1b594de1f6d57215818c3c48a609f761
Lana <lolhowko@gmail.com>
2024-08-28 05:47:42 +0300

    add slideshow and some corrects

--- slides.js
+++ slides.js
@@ -1,99 +1,76 @@
-// var slideRealIndex = 0;
-// showRealSlides();
+// var slideIndex = [1,1];
+// var slideId = ["projects__mySlides1", "projects__mySlides2"]
+// showSlides(1, 0);
+// showSlides(1, 1);
 
-// function showSlides() {
-//   var i;
-//   var slides = document.getElementsByClassName("mySlides");
-
-//   for (i = 0; i < slides.length; i++) {
-//     slides[i].style.display = "none";
-//   }
-//   slideRealIndex++;
-//   if (slideRealIndex > slides.length) {
-//     slideIndex = 1;
-//   }
-//   slides[slideIndex - 1].style.display = "block";
-//   setTimeout(showRealSlides, 3000); // Change image every 2 seconds
+// function plusSlides(n, no) {
+//   showSlides(slideIndex[no] += n, no);
 // }
 
+// function showSlides(n, no) {
+//   var i;
+//   var x = document.getElementsByClassName(slideId[no]);
+  
+//   if (n > x.length) {slideIndex[no] = 1}    
+//   if (n < 1) {slideIndex[no] = x.length}
+//   for (i = 0; i < x.length; i++) {
+//      x[i].style.display = "none";  
+//   }
+//   x[slideIndex[no]-1].style.display = "block";  
+// }
 
-// 2nd PAGE
+var slideIndex = 1;
+showSlides(slideIndex);
 
-var slideRealIndex = 1;
-showRealSlides(slideIndex);
-
-// Next/previous controls
 function plusSlides(n) {
-  showRealSlides((slideRealIndex += n));
+  showSlides(slideIndex += n);
 }
 
-// Thumbnail image controls
 function currentSlide(n) {
-  showRealSlides((slideRealIndex = n));
+  showSlides(slideIndex = n);
 }
 
-function showRealSlides(n) {
+function showSlides(n) {
   var i;
-  var slidesReal = document.getElementsByClassName("realization__mySlides");
-  var dotsReal = document.getElementsByClassName("realization__dot");
-  if (n > slidesReal.length) {
-    slideRealIndex = 1;
+  var slides = document.getElementsByClassName("projects__mySlides1");
+  var dots = document.getElementsByClassName("projects__dot1");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
   }
-  if (n < 1) {
-    slideRealIndex = slidesReal.length;
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
   }
-  for (i = 0; i < slidesReal.length; i++) {
-    slidesReal[i].style.display = "none";
-  }
-  for (i = 0; i < dotsReal.length; i++) {
-    dotsReal[i].className = dotsReal[i].className.replace(" active", "");
-  }
-  slidesReal[slideRealIndex - 1].style.display = "block";
-  dotsReal[slideRealIndex - 1].className += " active";
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
 }
 
 
-// 1st PAGE
+var slideIndex2 = 1;
+showSlides2(slideIndex2);
 
-var slideIndex = [1, 1];
-var dotsIndex = [1, 1];
-/* Class the members of each slideshow group with different CSS classes */
-var slideId = ["projects__mySlides", "projects__mySlides_2"];
-var dotsId = document.getElementsByClassName(
-  "projects__dot",
-  "projects__dot_2"
-);
-showSlides(1, 0);
-showSlides(1, 1);
-
-function plusSlides(n, no) {
-  showSlides((slideIndex[no] += n), no);
+function plusSlides2(n) {
+  showSlides2(slideIndex2 += n);
 }
 
-function showSlides(n, no) {
+function currentSlide2(n) {
+  showSlides2(slideIndex2 = n);
+}
+
+function showSlides2(n) {
   var i;
-  var x = document.getElementsByClassName(slideId[no]);
-  var d = document.getElementsByClassName(dotsId[no]);
-
-  if (n > x.length) {
-    slideIndex[no] = 1;
+  var slides2 = document.getElementsByClassName("projects__mySlides2");
+  var dots2 = document.getElementsByClassName("projects__dot2");
+  if (n > slides2.length) {slideIndex2 = 1}    
+  if (n < 1) {slideIndex2 = slides2.length}
+  for (i = 0; i < slides2.length; i++) {
+    slides2[i].style.display = "none";  
   }
-  if (n < 1) {
-    slideIndex[no] = x.length;
+  for (i = 0; i < dots2.length; i++) {
+    dots2[i].className = dots2[i].className.replace(" active", "");
   }
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
-  }
-  x[slideIndex[no] - 1].style.display = "block";
-
-  // if (n > d.length) {
-  //   dotsIndex[no] = 1;
-  // }
-  // if (n < 1) {
-  //   dotsIndex[no] = d.length;
-  // }
-  // for (i = 0; i < d.length; i++) {
-  //   x[i].style.display = "none";
-  // }
-  // d[dotsIndex[no] - 1].style.display = "block";
+  slides2[slideIndex2-1].style.display = "block";  
+  dots2[slideIndex2-1].className += " active";
 }
+
